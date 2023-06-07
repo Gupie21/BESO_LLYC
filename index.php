b020ae81774c275278f4a3e95962050bbc8eafcd
Gupie21 <gupie11722008@hotmail.com>
2023-06-06 20:04:57 -0600

    Visual quickfix

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <title></title>
   <meta name="description" content="BESO LLYC Prueba Técnica">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="icon" href="./favicon.ico">
   <link rel="stylesheet" href="app.css">
   
 </head>
